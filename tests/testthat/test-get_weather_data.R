f01729f8fa621534b05c26f78ae00567c07bdf23
Steffen Pentelow <steffen.pentelow@gmail.com>
2021-03-12 10:59:54 -0500

    Finish addting get_weather_data() test

--- tests/testthat/test-get_weather_data.R
+++ tests/testthat/test-get_weather_data.R
@@ -14,36 +14,18 @@ test_that("Dataframe should have 6 columns.", {
   expect_equal(ncol(weather_df),6)
 })
 
-# To be added
+test_that("Datetime columns should be represented as date time objects.", {
+  expect_equal(class(weather_df$datetime)[1],"POSIXct")
+  expect_equal(class(weather_df$datetime)[2],"POSIXt")
+})
 
+test_that("Datetime columns should be represented as date time objects.", {
+  expect_equal(class(weather_df$air_temp),"numeric")
+  expect_equal(class(weather_df$atm_press),"numeric")
+  expect_equal(class(weather_df$wind_spd),"numeric")
+  expect_equal(class(weather_df$wind_dir),"numeric")
+})
 
-# def test_column_datatypes():
-#   assert (
-#     weather_df.datetime.dtype == "<M8[ns]"
-#   ), "Data type of datetime column is incorrect (should be'<M8[ns]')."
-# assert (
-#   weather_df.air_temp.dtype == "float64"
-# ), "Data type of air_temp column is incorrect (should be 'float64')."
-# assert (
-#   weather_df.atm_press.dtype == "float64"
-# ), "Data type of atm_press column is incorrect (should be 'float64')."
-# assert (
-#   weather_df.wind_spd.dtype == "float64"
-# ), "Data type of wind_spd column is incorrect (should be 'float64')."
-# assert (
-#   weather_df.wind_dir.dtype == "float64"
-# ), "Data type of wind_dir column is incorrect (should be 'float64')."
-#
-#
-# def test_station_number_coding():
-#   assert (
-#     weather_df.stn.unique().shape[0] == 1
-#   ), "There should only be one station number in the data table"
-# assert (
-#   weather_df.stn.unique()[0] == station_number
-# ), "Station number should match entries values in stn column"
-#
-#
-# test_that("multiplication works", {
-#   expect_equal(2 * 2, 4)
-# })
+test_that("The only station number in the data table should be station_number", {
+  expect_equal(distinct(weather_df,stn)$stn,station_number)
+})
