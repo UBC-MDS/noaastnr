e76183a97f66a01f05991c10c7928beb4e4ce49b
Chen Zhao <chenzhaouc@gmail.com>
2021-03-13 00:47:44 -0700

    Finish plot_weather_data function and test, add dependencies

--- R/noaastnr.R
+++ R/noaastnr.R
@@ -40,7 +40,6 @@ get_stations_info <- function(country = "all") {
 #' @examples
 #' get_weather_data('911803-99999', 2015)
 get_weather_data <- function(station_number, year) {
-
   # Exception handling
   testthat::test_that("Year must be entered as a number", {
     testthat::expect_equal(class(year), "numeric")
@@ -52,7 +51,7 @@ get_weather_data <- function(station_number, year) {
     "Station number must be entered in form '911650-22536'.  See documentation for additional details.",
     {
       testthat::expect_equal(stringr::str_detect(station_number, "^[0-9]{6}[-][0-9]{5}$"),
-                   TRUE)
+                             TRUE)
     }
   )
 
@@ -118,7 +117,157 @@ get_weather_data <- function(station_number, year) {
 #' @export
 #'
 #' @examples
-#' plot_weather_data(obs_df, col_name = air_temp, time_basis = monthly)
+#' weather_df <- get_weather_data("911650-22536", 2020)
+#' plot_weather_data(obs_df = weather_df, col_name = "air_temp", time_basis = "monthly")
 plot_weather_data <- function(obs_df, col_name, time_basis) {
+  # Define global variables
+  datetime <-
+    air_temp <- atm_press <- wind_spd <- wind_dir <- month <- NULL
+  # Test input types
+  if (!is.data.frame(obs_df)) {
+    stop("Weather data should be a dataFrame.")
+  }
+  if (!is.character(col_name)) {
+    stop("Variable name must be entered as a factor.")
+  }
+  if (!is.character(time_basis)) {
+    stop("Time basis must be entered as a factor.")
+  }
+  # Test edge cases
+  testthat::test_that("Variable can only be one of air_temp, atm_press, wind_spd or wind_dir",
+                      {
+                        testthat::expect_true(col_name %in% c("air_temp", "atm_press", "wind_spd", "wind_dir"))
+                      })
+  testthat::test_that("Time basis can only be monthly or daily", {
+    testthat::expect_true(time_basis %in% c("monthly", "daily"))
+  })
 
+  df <- obs_df
+  df <- tidyr::drop_na(df)
+  if (nrow(df) < 3) {
+    stop("Dataset is not sufficient to visualize.")
+  }
+  year <-
+    lubridate::year(lubridate::floor_date(df$datetime, "year")[1])
+
+  if (time_basis == "monthly") {
+    df <-
+      dplyr::group_by(df, month = lubridate::floor_date(datetime, "month"))
+    df <-
+      dplyr::summarise(
+        df,
+        air_temp = mean(air_temp),
+        atm_press = mean(atm_press),
+        wind_spd = mean(wind_spd),
+        wind_dir = mean(wind_dir)
+      )
+
+    if (nrow(df) < 3) {
+      stop("Dataset is not sufficient to visualize.")
+    }
+
+    if (col_name == "air_temp") {
+      title_text <- paste("Air Temperature for ", year)
+      line <-
+        ggplot2::ggplot(df, ggplot2::aes(x = as.Date(month), y = air_temp)) +
+        ggplot2::geom_line(color = "orange") +
+        ggplot2::scale_x_date(date_labels = "%b", date_breaks = "1 month") +
+        ggplot2::ggtitle(title_text) +
+        ggplot2::xlab("Month") +
+        ggplot2::ylab("Air Temperature")
+    } else if (col_name == "atm_press") {
+      title_text <- paste("Atmospheric Pressure for ", year)
+      line <-
+        ggplot2::ggplot(df, ggplot2::aes(x = as.Date(month), y = atm_press)) +
+        ggplot2::geom_line(color = "orange") +
+        ggplot2::scale_x_date(date_labels = "%b", date_breaks = "1 month") +
+        ggplot2::ggtitle(title_text) +
+        ggplot2::xlab("Month") +
+        ggplot2::ylab("Atmospheric Pressure")
+    } else if (col_name == "wind_spd") {
+      title_text <- paste("Wind Speed for ", year)
+      line <-
+        ggplot2::ggplot(df, ggplot2::aes(x = as.Date(month), y = wind_spd)) +
+        ggplot2::geom_line(color = "orange") +
+        ggplot2::scale_x_date(date_labels = "%b", date_breaks = "1 month") +
+        ggplot2::ggtitle(title_text) +
+        ggplot2::xlab("Month") +
+        ggplot2::ylab("Wind Speed")
+    } else {
+      title_text <- paste("Wind Direction for ", year)
+      line <-
+        ggplot2::ggplot(df, ggplot2::aes(x = as.Date(month), y = wind_dir)) +
+        ggplot2::geom_line(color = "orange") +
+        ggplot2::scale_x_date(date_labels = "%b", date_breaks = "1 month") +
+        ggplot2::ggtitle(title_text) +
+        ggplot2::xlab("Month") +
+        ggplot2::ylab("Wind Direction")
+    }
+
+  } else {
+    df <-
+      dplyr::group_by(df, month = lubridate::floor_date(datetime, "day"))
+    df <-
+      dplyr::summarise(
+        df,
+        air_temp = mean(air_temp),
+        atm_press = mean(atm_press),
+        wind_spd = mean(wind_spd),
+        wind_dir = mean(wind_dir)
+      )
+
+    if (nrow(df) < 3) {
+      stop("Dataset is not sufficient to visualize.")
+    }
+
+    if (col_name == "air_temp") {
+      title_text <- paste("Air Temperature for ", year)
+      line <-
+        ggplot2::ggplot(df, ggplot2::aes(x = as.Date(date), y = air_temp)) +
+        ggplot2::geom_line(color = "orange") +
+        ggplot2::scale_x_date(date_labels = "%b", date_breaks = "1 month") +
+        ggplot2::ggtitle(title_text) +
+        ggplot2::xlab("Date") +
+        ggplot2::ylab("Air Temperature")
+    } else if (col_name == "atm_press") {
+      title_text <- paste("Atmospheric Pressure for ", year)
+      line <-
+        ggplot2::ggplot(df, ggplot2::aes(x = as.Date(date), y = atm_press)) +
+        ggplot2::geom_line(color = "orange") +
+        ggplot2::scale_x_date(date_labels = "%b", date_breaks = "1 month") +
+        ggplot2::ggtitle(title_text) +
+        ggplot2::xlab("Date") +
+        ggplot2::ylab("Atmospheric Pressure")
+    } else if (col_name == "wind_spd") {
+      title_text <- paste("Wind Speed for ", year)
+      line <-
+        ggplot2::ggplot(df, ggplot2::aes(x = as.Date(date), y = wind_spd)) +
+        ggplot2::geom_line(color = "orange") +
+        ggplot2::scale_x_date(date_labels = "%b", date_breaks = "1 month") +
+        ggplot2::ggtitle(title_text) +
+        ggplot2::xlab("Date") +
+        ggplot2::ylab("Wind Speed")
+    } else {
+      title_text <- paste("Wind Direction for ", year)
+      line <-
+        ggplot2::ggplot(df, ggplot2::aes(x = as.Date(date), y = wind_dir)) +
+        ggplot2::geom_line(color = "orange") +
+        ggplot2::scale_x_date(date_labels = "%b", date_breaks = "1 month") +
+        ggplot2::ggtitle(title_text) +
+        ggplot2::xlab("Date") +
+        ggplot2::ylab("Wind Direction")
+    }
+  }
+
+  chart <- line +
+    ggplot2::theme_bw() +
+    ggplot2::theme(
+      text = ggplot2::element_text(size = 13),
+      plot.title = ggplot2::element_text(size = 22),
+      panel.grid.major = ggplot2::element_blank(),
+      panel.grid.minor = ggplot2::element_blank(),
+      panel.border = ggplot2::element_blank(),
+      axis.line = ggplot2::element_line(colour = "black")
+    )
+  chart
 }
--- tests/testthat/test-plot_weather_data.R
+++ tests/testthat/test-plot_weather_data.R
@@ -1,3 +1,85 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+station_number <-  "911650-22536"
+year <- 2020
+weather_df <- get_weather_data(station_number, year)
+
+plot_at_m <-
+  plot_weather_data(obs_df = weather_df,
+                    col_name = "air_temp",
+                    time_basis = "monthly")
+plot_ap_m <-
+  plot_weather_data(obs_df = weather_df,
+                    col_name = "atm_press",
+                    time_basis = "monthly")
+plot_ws_m <-
+  plot_weather_data(obs_df = weather_df,
+                    col_name = "wind_spd",
+                    time_basis = "monthly")
+plot_wd_m <-
+  plot_weather_data(obs_df = weather_df,
+                    col_name = "wind_dir",
+                    time_basis = "monthly")
+plot_at_d <-
+  plot_weather_data(obs_df = weather_df,
+                    col_name = "air_temp",
+                    time_basis = "daily")
+plot_ap_d <-
+  plot_weather_data(obs_df = weather_df,
+                    col_name = "atm_press",
+                    time_basis = "daily")
+plot_ws_d <-
+  plot_weather_data(obs_df = weather_df,
+                    col_name = "wind_spd",
+                    time_basis = "daily")
+plot_wd_d <-
+  plot_weather_data(obs_df = weather_df,
+                    col_name = "wind_dir",
+                    time_basis = "daily")
+
+# Test mapped axis
+
+test_that("Month should be mapped to the x axis", {
+  expect_true("as.Date(month)" == rlang::get_expr(plot_at_m$mapping$x))
+  expect_true("as.Date(month)" == rlang::get_expr(plot_ap_m$mapping$x))
+  expect_true("as.Date(month)" == rlang::get_expr(plot_ws_m$mapping$x))
+  expect_true("as.Date(month)" == rlang::get_expr(plot_wd_m$mapping$x))
+})
+
+test_that("Date should be mapped to the x axis", {
+  expect_true("as.Date(date)" == rlang::get_expr(plot_at_d$mapping$x))
+  expect_true("as.Date(date)" == rlang::get_expr(plot_ap_d$mapping$x))
+  expect_true("as.Date(date)" == rlang::get_expr(plot_ws_d$mapping$x))
+  expect_true("as.Date(date)" == rlang::get_expr(plot_wd_d$mapping$x))
+})
+
+test_that("air_temp should be mapped to the y axis", {
+  expect_true("air_temp" == rlang::get_expr(plot_at_m$mapping$y))
+  expect_true("air_temp" == rlang::get_expr(plot_at_d$mapping$y))
+})
+
+test_that("atm_press should be mapped to the y axis", {
+  expect_true("atm_press" == rlang::get_expr(plot_ap_m$mapping$y))
+  expect_true("atm_press" == rlang::get_expr(plot_ap_d$mapping$y))
+})
+
+test_that("wind_spd should be mapped to the y axis", {
+  expect_true("wind_spd" == rlang::get_expr(plot_ws_m$mapping$y))
+  expect_true("wind_spd" == rlang::get_expr(plot_ws_d$mapping$y))
+})
+
+test_that("wind_dir should be mapped to the y axis", {
+  expect_true("wind_dir" == rlang::get_expr(plot_wd_m$mapping$y))
+  expect_true("wind_dir" == rlang::get_expr(plot_wd_d$mapping$y))
+})
+
+# Test plot configurations
+
+test_that("plot should use geom_line", {
+  expect_true("GeomLine" %in% c(class(plot_at_m$layers[[1]]$geom)))
+  expect_true("GeomLine" %in% c(class(plot_ap_m$layers[[1]]$geom)))
+  expect_true("GeomLine" %in% c(class(plot_ws_m$layers[[1]]$geom)))
+  expect_true("GeomLine" %in% c(class(plot_wd_m$layers[[1]]$geom)))
+  expect_true("GeomLine" %in% c(class(plot_at_d$layers[[1]]$geom)))
+  expect_true("GeomLine" %in% c(class(plot_ap_d$layers[[1]]$geom)))
+  expect_true("GeomLine" %in% c(class(plot_ws_d$layers[[1]]$geom)))
+  expect_true("GeomLine" %in% c(class(plot_wd_d$layers[[1]]$geom)))
 })
